9af659711774bfbdde5c75df708083ec1b85eef3
Soumyajit <mondalsoumya02@gmail.com>
2023-08-28 17:46:24 +0530

    Final Changes

--- Frontend/src/Pages/Home/FAQ/FAQ.jsx
+++ Frontend/src/Pages/Home/FAQ/FAQ.jsx
@@ -8,62 +8,37 @@ const FAQ = () => {
                 <Accordion.Item eventKey="0" >
                     <Accordion.Header >Install Telegram on Device</Accordion.Header>
                     <Accordion.Body className="bg-gray-200">
-                        Install Telegram on your device from <a href="https://telegram.org/">here</a> or use Telegram Web from <a href="https://web.telegram.org/">here</a>
+                        Install Telegram on your device from <a href="https://telegram.org/">HERE</a> or use Telegram Web from <a href="https://web.telegram.org/">HERE</a>
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="1">
                     <Accordion.Header>Add Blood Sphere Bot to Telegram</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        Click on <a href="https://t.me/blood_sphere_bot">Blood Sphere Bot</a> to add it to your Telegram account.
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="2">
                     <Accordion.Header>Use /start to start interacting with the bot</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        Use /start command to start the BOT. There will be 2 options to choose. You can either search for blood or donate blood.
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="3">
                     <Accordion.Header>Use /get to search Blood Groups</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        Use /get command to search for blood groups. You can search for blood groups by typing the blood group name.
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="4">
                     <Accordion.Header>Use /donate to search for nearby Blood Donation Camps</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        Use /donate command to search for nearby blood donation camps. You can search for blood donation camps by typing the pincode number of that area.
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="5">
                     <Accordion.Header>Enter pincode number of that area where you're finding for blood</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        Enter the pincode number of that area where you're finding for blood. You will get the list of Hospitals and Blood Donation Camps that are available in that area.
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
